ef09e34e76ff73e2640924d556f0f9847fc16e63
pavel <triodjangopiter@yandex.ru>
2023-08-29 12:26:54 +0300

    rename

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from "@angular/router";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
-import { DateTimeFormatPipe } from "./pispes";
+import { DateTimeFormatPipe } from "./pipes";
 
 @NgModule({
   declarations: [
